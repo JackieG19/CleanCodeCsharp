c49e5d027fecc226f013e24a8251cc0453890fd3
Jackie <JGtheophila19@gmail.com>
2018-11-28 19:12:50 -0500

    Store multiple values in 1 variable using arrays

---
+++ storevalue.js
@@ -0,0 +1,12 @@
+/*
+Modify the new array myArray so that it contains both a string and a number (in that order).
+myArray should be an array.
+The first item in myArray should be a string.
+The second item in myArray should be a number.
+*/
+
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["hi", 5];
