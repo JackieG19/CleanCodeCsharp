0e555fa5e81a424c0b03e81fe95bec4f93a69c0e
Jackie <JGtheophila19@gmail.com>
2019-09-30 13:23:44 -0400

    Declares variable or assign the value to var

---
+++ Variables/integer.cs
@@ -0,0 +1,12 @@
+// Declares a variable named myAge of type integer
+int myAge;
+
+// Assign the value of a variable when you declare it:
+int myAge = 18;
+
+
+// Example: To declare a variable named num of type integer and assign 42 to it
+   
+int num;
+num = 42;
+
