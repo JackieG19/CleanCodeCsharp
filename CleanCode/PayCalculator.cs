6e9148c71b1888cd5896127636dc70707f14c4ef
Jackie <JGtheophila19@gmail.com>
2020-04-01 15:24:04 -0400

    Create PayCalculator.cs

---
+++ CleanCode/PayCalculator.cs
@@ -0,0 +1,51 @@
+using System;
+
+namespace CleanCode
+{
+    public decimal CalcGross(decimal rate, decimal hours)
+    {
+        decimal overtimeHours = 0;
+        decimal regularHours = 0;
+        decimal regularPay = 0;
+        decimal overtimePay = 0;
+        decimal grossPay = 0;
+        
+        if(_payFrequency == PayFrequency.Fortnightly)
+        {
+            if(hours > 8)
+            {
+                overtimeHours = hours - 80;
+                regularHours = 80;
+            }
+            
+            else
+                regularHours = hours;
+        }
+        
+        else if(_payFrequency == PayFrequency.Weekly)
+        {
+            if(hours > 40)
+            {
+                overtimeHours = hours - 40;
+                regularHours = 40;
+            }
+            
+            else
+                regularHours = hours;
+        }
+        
+        if(overtimeHours > 0m)
+        {
+            overtimePay += (rate * 1.5m) * overtimeHours;
+        }
+        regularPay = (regularHours * rate);
+        grossPay = regularPay + overtimePay;
+        return grossPay;
+    }
+    
+    public enum PayFrequency
+    {
+        Weekly,
+        Fortnightly
+    }
+}
