7051b7d47a3fdd1ee833edb68c33726c5bc04410
Jackie <JGtheophila19@gmail.com>
2020-04-01 15:02:08 -0400

    returning an object from the method instead

--- CleanCode/OutputParameters.cs
+++ CleanCode/OutputParameters.cs
@@ -7,26 +7,28 @@ namespace CleanCode
     {
         public class GetCustomersResult
         {
-            public IEnumerable<Customer> Items1 { get; set; }
-            public int Items2 { get; set; }
+            public IEnumerable<Customer> Customers { get; set; }
+            public int TotalCount { get; set; }
         }
         
         public void DisplayCustomers()
         {
-            int totalCount = 0;
-            var tulpe = GetCustomers(1);
-            totalCount = tulpe.Items2;
-            var customers = tulpe.Items1;
+            //int totalCount = 0;
+            //const int pageIndex = 1;
+            var result = GetCustomers(pageIndex: 1);
             
-            Console.WriteLine("Total customers: " + totalCount);
-            foreach(var c in customers)
+            //totalCount = result.TotalCount;
+            //var customers = result.Customers;
+            
+            Console.WriteLine("Total customers: " + result.TotalCount);
+            foreach(var c in result.Customers)
               Console.WriteLine(c);
         }
         
-        public Tulpe<IEnumerable<Customer>, int> GetCustomers(int pageIndex)
+        public GetCustomersResult GetCustomers(int pageIndex)
         {
             var totalCount = 100;
-            return new GetCustomersResult() { Item1 = new List<Customer>(), Item2 = totalCount}
+            return new GetCustomersResult() { Customers = new List<Customer>(), TotalCount = totalCount}
         }
     }
 }
