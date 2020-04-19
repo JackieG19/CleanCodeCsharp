61687d030f21fd8ebdb6d0219e57a72aa3a70378
Jackie <JGtheophila19@gmail.com>
2020-04-19 12:48:09 -0400

    Create MagicNumbers.cs

---
+++ CleanCode/MagicNum/MagicNumbers.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace CleanCode
+{
+    public class MagicNumbers
+    {
+        public void ApproveDocument(int status)
+        {
+            if(status == 1)
+            {
+                // ...
+            }
+            else if(status == 2)
+            {
+                // ...
+            }
+        }
+        
+        public void RejectDocument(string status)
+        {
+            switch (status)
+            {
+                case "1":
+                    // ...
+                    break;
+                case "2":
+                    // ...
+                    break;
+            }
+        }
+    }
+}
