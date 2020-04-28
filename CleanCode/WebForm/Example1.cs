25675186862dcbfe199f876d0497adbcb4099cf9
Jackie <JGtheophila19@gmail.com>
2020-04-28 11:58:12 -0400

    Update Example1.cs

--- CleanCode/WebForm/Example1.cs
+++ CleanCode/WebForm/Example1.cs
@@ -4,67 +4,84 @@ using System.Collection.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Web.UI.WebControls;
+using Project.UserControls;
 
-namespace Project.UserControls
+namespace CleanCode.FullRefactoring
 {
     public class PostControl : System.Web.UI.UserControls
     {
-        private PostDbContext DBContext;
+        private readonly PostRepository _postRepository;
+        private readonly PostValidator _validator;
+        public Label PostBody { get; set; }
+        public Label PostTitle { get; set; }
+        public int? PostId { get; set; }
+        
+        public PostControl()
+        {
+            _postRepository = new PostRepository();
+             _validator = new PostValidator();
+        }
         
         protected void Page_Load(object sender, EventArgs e)
-        {
-            DBContext = PostDbContext();
-            
+        {   
             if(Page.IsPostBack)
-            {
-                PostValidator validator = PostValidator();
-                Post entity = new Post()
-                {
-                    // Map from fields to entity properties
-                    Id = Convert.ToInt32(PostId.Value),
-                    Title = PostTitle.Text.Trim(),
-                    Body = PostBody.Text.Trim()
-                };
-                
-                ValidtionResult results = validator.Validator(entity);
-                
-                if(results.IsValid)
-                {
-                    // save to the database and continue to the next page
-                    DBContext = Post.Add(entity);
-                    DBContext = SaveChange();
-                }
-                
-                else
-                {
-                    BulletedList summary = (BulletedList)FindControl("ErrorSummary"));
-                    
-                    // Display errors to the user
-                    foreach(var failure in results.Error)
-                    {
-                        Label errorMessage = FindControl(failure.PropertyName + "Error") as Label;
-                        
-                        if(errorMessage == null)
-                        {
-                            summary.Items.Add(new ListItem(failure.ErrorMessage));
-                        }
-                        else
-                        {
-                            errorMessage.Text = failure.ErrorMessage
-                        }
-                    }
-                }
-            }
+                TrySavePost();
             else
+                DisplayPost();
+        }
+        
+        private void TrySavePost()
+        {   
+            var entity = GetPost();
+            var results = _validator.Validator(entity);
+            
+            if(results.IsValid)
+                _postRepository.SavePost(entity);
+            else
+                DisplayError(results);
+        }
+        
+        private void DisplayError(ValidationResult results)
+        {
+            var summary = GetErrorSummaryControl();
+
+            foreach(var error in results.Error)
             {
-                // display form
-                Post entity = DBContext.Post.SingleOrDefault(p => p.Id == Convert.ToInt32(Request.QueryString["id"]));
-                PostBody.Text = entity.Body;
-                PostTitle.Text = entity.Title;
+                var label = GetErrorLabel(error);
+                
+                if(label == null)
+                    summary.Items.Add(new ListItem(error.ErrorMessage));
+                else
+                    label.Text = error.ErrorMessage;
             }
         }
-        public Label PostBody { get; set; }
         
-        public Label PostTitle { get; set; }
+        private BulletedList GetErrorSummaryControl()
+        {
+            return (BulletedList) FindControl("ErrorSummary");
+        }
+        
+        private Label GetErrorLabel(ValidationError error)
+        {
+            return FindControl(error.PropertyName + "Error") as Label;
+        }
+        
+        private void DisplayPost()
+        {
+            int postId = Convert.ToInt32(Request.QueryString["id"]);
+            var post = _postRepository.GetPost(postId);
+            PostBody.Text = post.Body;
+            PostTitle.Text = post.Title;
+        }
+        
+        private Post GetPost()
+        {
+            return new Post()
+            {
+                Id = Convert.ToInt32(PostId.Value),
+                Title = PostTitle.Text.Trim(),
+                Body = PostBody.Text.Trim()
+            };
+        }
     }
 }
