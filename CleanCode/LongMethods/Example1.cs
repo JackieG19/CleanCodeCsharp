81798f7bdb79e76fd8af30137d4b44698b8887a7
Jackie <JGtheophila19@gmail.com>
2020-04-27 20:14:27 -0400

    Update Example1.cs

--- CleanCode/LongMethods/Example1.cs
+++ CleanCode/LongMethods/Example1.cs
@@ -13,91 +13,50 @@ namespace FooFoo
 {
     public partial class Download : System.Web.UI.Page
     {
+        private readonly DataTableToCsvMapper _dataTableToCsvMapper = new DataTableToCsvMapper();
+        private readonly TableReader _tableReader = new TableReader();
+        
         protected void Page_Load(object sender, EventArgs e)
         {
-            System.IO.MemoryStream ms = CreateMemoryFile();
+            ClearResponse();
+            SetCacheability();
+            WriteContentToResponse(byteArray);
+        } 
+        
+        pirvate byte[] GetCsv()
+        {
+            System.IO.MemoryStream ms = _dataTableToCsvMapper.Map(_tableReader.GetDataTable());
             
             byte[] byteArray = ms.ToArray();
             ms.Flush();
             ms.Close();
+        }
             
-            Response.Clear();
-            Response.ClearContent();
-            Response.ClearHeaders();
-            Response.Cookies.Clear();
-            Response.Cache.SetCacheability(HttpCacheability.Private);
-            Response.CacheControl = "private";
+        private void WriteContentToResponse(byte[] byteArray)
+        {
             Response.Charset = System.Text.UTF8Encoding.UTF8.WebName;
             Response.ContentEncoding = System.Text.UTF8Encoding.UTF8;
-            Response.AppendHeder("Pragma", "cache");
-            Response.AppendHeder("Expires", "60");
+            
             Response.ContentType = "text/comma-separated-values";
             Response.AppendHeder("Content-Disposition", "attachment; filename=FooFoo.csv");
             Response.AppendHeder("Content-Length", byteArray.Length.ToString());
             Response.BinaryWrite(byteArray);
         }
-        
-        public System.IO.MemoryStream CreateMemoryFile()
-        {
-            MemoryStream ReturnStream = new MemoryStream();
             
-            try
-            {
-                string strConn = ConfigurationManager.ConnectionStrings["FooFooConnectionString"].ToString();
-                SqlConnection conn = new SqlConnection(strConn);
-                SqlDataAdapter da = new SqlConnection("SELECT * FROM [FooFoo] ORDER BY id ASC", conn);
-                DataSet ds = new DataSet();
-                da.Fill(ds, "FooFoo");
-                DataTable dt = ds.Tables["FooFoo"];
-                
-                // create a streamwriter to writer to the memory stream
-                StreamWriter sw = new StreamWriter(ReturnStream);
-                
-                int iColCount = dt.Columns.Count;
-                
-                for(int i = 0; i < iColCount; i++)
-                {
-                    sw.Write(dt.Columns[i]);
-                    
-                    if(i < iColCount - 1)
-                    {
-                        sw.Write(",");
-                    }
-                }
-                
-                sw.Write();
-                int intRows = dt.Rows.Count
-                
-                // Now write all the rows
-                foreach(DataRow dr int dt.Rows)
-                {
-                    for(int i = 0; i < iColCount; i++)
-                    {
-                        if(!Convert.IsDBNull(dr[i]))
-                        {
-                            string str = String.Format("\"{0:c}\"", dr[i].ToString()).Replace("\r\n", " ");
-                        }
-                        
-                        else
-                        {
-                            sw.Write("");
-                        }
-                        
-                        if(i < iColCount - 1)
-                        {
-                            sw.Write(",");
-                        }
-                    }
-                    sw.WriteLine();
-                }
-                sw.Flush();
-                sw.Close();
-            }
-            catch(Expection Ex)
-            {
-                throw Ex;
-            }
-            return ReturnStream;
+        private void ClearResponse()
+        {
+            Response.Cache.SetCacheability(HttpCacheability.Private);
+            Response.CacheControl = "private";
+            Response.AppendHeder("Pragma", "cache");
+            Response.AppendHeder("Expires", "60");
+        }
+            
+        private void SetCacheability()
+        {
+            Response.Clear();
+            Response.ClearContent();
+            Response.ClearHeaders();
+            Response.Cookies.Clear();
         }
     }
 }
